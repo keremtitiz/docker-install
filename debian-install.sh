47498c29a07ca121dcf9aec1d303cbe6932b7fcb
Kerem Titiz <keremtitiz@gmail.com>
2022-12-05 11:28:55 +0300

    Create debian-install.sh

---
+++ debian-install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo " Docker Ce - Debian Installerv"
+apt update
+apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+apt update
+apt install docker-ce -y
+systemctl enable docker
+systemctl start docker
